d39e207e72317e6c72371cf519ebab0bbad4f5d2
GitHub Action <action@github.com>
2022-06-03 21:44:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/drkcode/clasicharmonizedforintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.drkcode.clasicharmonizedforintellij.services
+
+import com.github.drkcode.clasicharmonizedforintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
